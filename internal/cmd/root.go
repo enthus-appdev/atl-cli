3921fda164e5615ca144ac9b78431ca93e935385
Florian Kinder <florian.kinder@enthus.de>
2026-04-12 13:09:28 +0200

    fix: append -dirty suffix when built from modified worktree

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -81,6 +81,7 @@ func newVersionCmd(ios *iostreams.IOStreams, version, commit, date string) *cobr
 
 func vcsInfo() (commit, date string) {
 	commit, date = "unknown", "unknown"
+	var modified bool
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
 		return
@@ -88,14 +89,19 @@ func vcsInfo() (commit, date string) {
 	for _, s := range info.Settings {
 		switch s.Key {
 		case "vcs.revision":
-			if len(s.Value) >= 7 {
+			if len(s.Value) > 7 {
 				commit = s.Value[:7]
 			} else {
 				commit = s.Value
 			}
 		case "vcs.time":
 			date = s.Value
+		case "vcs.modified":
+			modified = s.Value == "true"
 		}
+	}
+	if modified {
+		commit += "-dirty"
 	}
 	return
 }
