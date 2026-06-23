abef0126f423773f4903868e6f50441f76037b5f
Hinne Stolzenberg <hinne.stolzenberg@enthus.de>
2026-06-23 07:31:33 +0200

    feat: atl jira sprint start

--- internal/cmd/sprint/sprint.go
+++ internal/cmd/sprint/sprint.go
@@ -76,9 +76,6 @@ func printSprint(ios *iostreams.IOStreams, s *api.Sprint, asJSON bool) error {
 }
 
 // --- temporary stubs (removed as each command is implemented) ---
-func newCmdStart(ios *iostreams.IOStreams) *cobra.Command {
-	return &cobra.Command{Use: "start", Hidden: true, RunE: func(*cobra.Command, []string) error { return nil }}
-}
 func newCmdClose(ios *iostreams.IOStreams) *cobra.Command {
 	return &cobra.Command{Use: "close", Hidden: true, RunE: func(*cobra.Command, []string) error { return nil }}
 }
